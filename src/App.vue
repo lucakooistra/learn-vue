638504e3cc0665c573f2145177d43d192ccf6f6c
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-07-22 14:09:20 +0200

    feat: Add JobListings component and update App.vue
    The commit adds a new JobListings component to the project and updates App.vue to include the JobListings component. This allows for displaying job listings on the home page.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero title="Test title" subtitle="Test Subtitle"/>
   <HomeCards/>
+  <JobListings/>
 </template>
